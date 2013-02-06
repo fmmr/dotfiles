041d54ff2c5ae3934c58e9db2d6054d170504af7
frerodla <fmr@finn.no>
2013-02-06 21:08:40 +0100

    testing if brew exists before calling it

--- 5_completions.sh
+++ 5_completions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 complete -A alias unalias alias
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-  . `brew --prefix`/etc/bash_completion
+
+brew --prefix >/dev/null 2>&1
+if [ $? -eq 0 ]; then 
+	if [ -f `brew --prefix`/etc/bash_completion ]; then
+	  . `brew --prefix`/etc/bash_completion
+	fi
 fi
