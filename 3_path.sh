9e17eb77169ee729bfce2a04baea905f6838cc6b
frerodla <fmr@finn.no>
2013-02-06 20:57:46 +0100

    rm original before linking in link_files.sh; own file for path

---
+++ 3_path.sh
@@ -0,0 +1,13 @@
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/libexec/:$PATH
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$SCALA_HOME/bin
+export PATH=$PATH:$M2:$JAVA_HOME/bin
+export PATH=$PATH:$PLAY_HOME
+
+#export	PATH=/Applications/Sybase/System/OCS-12_5/bin:/Applications/Sybase/System/ASE-12_5/bin:$PATH
+#export PATH=$PATH:$HOME/.rvm/bin
+#export PATH=$PATH:/usr/local/jasypt/bin
+#export PATH=$PATH:/usr/local/activemq/bin
+#export PATH=$PATH:/usr/local/mongodb/bin
+#export PATH=$PATH:/usr/local/node/bin
+
