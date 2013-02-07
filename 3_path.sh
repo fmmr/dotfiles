b3e030542564c0d157248b141489995b4c4a35a5
Fredrik Rødland <github@rodland.no>
2013-02-07 15:10:16 +0100

    added bin/ferdig to path

--- 3_path.sh
+++ 3_path.sh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/libexec/:$PATH
+export PATH=$HOME/bin:$HOME/bin/ferdig:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/libexec/:$PATH
 export PATH=$PATH:$HADOOP_HOME/bin
 export PATH=$PATH:$SCALA_HOME/bin
 export PATH=$PATH:$M2:$JAVA_HOME/bin
