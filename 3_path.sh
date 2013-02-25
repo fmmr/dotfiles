d4234634f40fc16f81388545239d3cd50f43dd67
Fredrik Rødland <github@rodland.no>
2013-02-25 15:58:17 +0100

    added local in-path-directory and added livelog from @tlberglund

--- 3_path.sh
+++ 3_path.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:$HADOOP_HOME/bin
 export PATH=$PATH:$SCALA_HOME/bin
 export PATH=$PATH:$M2:$JAVA_HOME/bin
 export PATH=$PATH:$PLAY_HOME
+export PATH=$PATH:$DOTFILES/9_in_path
 
 #export	PATH=/Applications/Sybase/System/OCS-12_5/bin:/Applications/Sybase/System/ASE-12_5/bin:$PATH
 #export PATH=$PATH:$HOME/.rvm/bin
