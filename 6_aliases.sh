04efd79423bd0546b47e337265a846a3418b8887
Fredrik Rødland <github@rodland.no>
2013-02-28 09:37:56 +0100

    cleaned aliases and added wd-alias to config

--- 6_aliases.sh
+++ 6_aliases.sh
@@ -12,12 +12,8 @@ alias 'j6=export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)'
 alias 'j7=export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)'
 alias 'j8=export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
 alias 'kff=killall firefox firefox-bin'
-alias 'l=ls -al'
-alias 'l=ls -alF'
 alias 'latest=tail -F ~/bin/log/latest'
 alias 'less=less -Mi'
-alias 'ls=ls -h'
-alias 'lt=ls -ltr'
 alias 'mci=mvn clean install'
 alias 'md=mkdir'
 alias 'mvn=mvn -DdownloadSources=true -DdownloadJavadocs=true'
@@ -32,13 +28,19 @@ alias 't=tail -f /Users/fmr/bin/log/rsync.log'
 alias 'where=type -all'
 alias 'x=xargs'
 alias check_net="ping vg.no | awk -F = {'print \$4'}"
-# FINN
+
+# FINN-ing
 alias 'gits=cd ~/finn/search && for i in `ls`; do echo "----CHECKING $i----"; cd $i; git s; cd ..; done'
 alias 'gitu=cd ~/finn/search && for i in `ls`; do echo "----UPDATING $i----"; cd $i; git pull; cd ..; done'
+# courteously of @gardleopard
 alias 'supergit=git pull && git add -A && git commit -m "supercommit from fmr `date +%s`" && git push -f'
 
+# LS-ing
+alias 'ls=ls -h'
+alias 'l=ls -alF'
+alias 'lt=ls -ltr'
 
-# SSH-ING
+# SSH-ing
 for i in `grep "^Host" ~/.ssh/config | grep -v "Host \*" | awk '{print $2, $3, $4, $5, $6, $7, $8}'`; do alias $i="ssh $i"; done
 
 # CD-ing
