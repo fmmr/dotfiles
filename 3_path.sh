08ca70511cfc8435c74a47349bc90159976b46e9
frerodla <fmr@finn.no>
2014-09-12 14:43:54 +0200

    added sub-dirs of bin to path

--- 3_path.sh
+++ 3_path.sh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:$HOME/bin/ferdig:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/libexec:/root/bin:$PATH
+export PATH=$HOME/bin:$HOME/bin/ferdig:$HOME/bin/maisen:$HOME/bin/rod:$HOME/bin/finn:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/libexec:/root/bin:$PATH
 export PATH=$PATH:/usr/local/node/bin
 export PATH=$PATH:$HADOOP_HOME/bin
 export PATH=$PATH:$SCALA_HOME/bin
